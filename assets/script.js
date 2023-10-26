bf857ae6044b86242fcc565d94e6928f60dfd460
chris <davischris25139@yahoo.com>
2023-10-26 16:45:03 -0700

    added API feature for search bar car model, look under console logs for response. added eventlistener for search bar button, added script to jquery under HTML file.

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+
+var options = {
+    method: 'GET',
+    headers: { 'X-Api-Key': 'bHmMS6Y1Uh7qh8GEsYOYsA==27pfSz2CXKjlH5w4'},
+}
+$('.ui button').on('click', function(){
+    var textInput = $('.ui button').siblings('.userInput').val()
+    var model = textInput
+    var url = 'https://api.api-ninjas.com/v1/cars?limit=20&model='+model
+    
+    fetch(url,options)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log('error ${err}')
+    })
+})
+
