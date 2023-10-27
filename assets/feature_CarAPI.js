e5d25a50f177b18d3e1e4ee820212c8693a76696
chris <davischris25139@yahoo.com>
2023-10-26 23:13:23 -0700

    added feature to append information to page, also removes prior to next search!

--- assets/feature_CarAPI.js
+++ assets/feature_CarAPI.js
@@ -1,19 +1,52 @@
-
+function searchCars() {
+  
 var options = {
   method: "GET",
   headers: { "X-Api-Key": "bHmMS6Y1Uh7qh8GEsYOYsA==27pfSz2CXKjlH5w4" },
 };
-$(".ui button").on("click", function () {
-  var textInput = $(".ui button").siblings(".userInput").val();
+$(".car-btn").on("click", function () {
+  var textInput = $(".car-btn").siblings(".userInput").val();
   var model = textInput;
-  var url = "https://api.api-ninjas.com/v1/cars?limit=20&model=" + model;
+  var url = "https://api.api-ninjas.com/v1/cars?limit=5&model=" + model;
+
+  $(".results").empty();
 
   fetch(url, options)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
+  .then(function(resp){
+    return resp.json()
+  })
+  .then(function(data){
+    var results = data
+    console.log(results)
+    results.forEach(function({make, model, year, fuel_type, cylinders, drive}){
+      var card = document.createElement('div')
+      card.classList.add('card')
+      card.innerHTML = `
+      <div class="card-body">
+        <h3 class="card-make">${make}, ${model}</h3>
+        <h4 class="card-subtitle mb-2 text-body-secondary">Fuel Type: ${fuel_type}, Cylinders: ${cylinders}, Drive: ${drive}</h4>
+        <p class="card-text">Year: ${year}</p>
+        <br>
+      </div>
+      `
+      
+      document.querySelector('.results').appendChild(card)
     })
-    .catch((err) => {
-      console.log("error ${err}");
-    });
-});
+  })
+  });
+}
+searchCars()
+
+
+
+
+//   fetch(url, options)
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//         var results = data
+//         console.log(results)
+//       //  results.forEach(function({make, model, year, fuel_type, cylinders, drive}))
+//     })
+//     .catch((err) => {
+//       console.log("error ${err}");
+//     });
